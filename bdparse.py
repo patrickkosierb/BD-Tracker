9808acf161bbc25ffcdc7f6e655c63d2fcb2a6f6
patrickkosierb <kosierbp@mcmaster.ca>
2022-11-03 21:08:45 -0400

    created methods

--- bdparse.py
+++ bdparse.py
@@ -5,69 +5,82 @@
 import subprocess
 import datetime
 
+class BDError:
+    __OK =                  1
+    __PREV_LOG =            1
+    __ERROR_SUBPROCESS =    251
+    __ERROR_EXIST =         404
+    __ERROR_UNKNOWN =       -1
 
-## Return marcros ##
-BD_OK =                  0
-BD_PREV_LOG =            1
-BD_ERROR_SUBPROCESS =    251
-BD_ERROR_EXIST =         404
-BD_ERROR_UNKNOWN =       -1
+    def OK(self):
+        return self.__OK
+    def PREV_LOG(self):
+        return self.__PREV_LOG
+    def ERROR_SUBPROCESS(self):
+        return self.__ERROR_SUBPROCESS
+    def ERROR_EXIST(self):
+        return self.__ERROR_EXIST
+    def ERROR_UNKNOWN(self):
+        return self.__ERROR_UNKNOWN
 
-def check_bd(driver, bios):
-    try:
-        f = open("B-D_log.txt","r")
-        sysinfo = []
-        for element in range(0,4):
-            line = f.readline()
-            if(element>=2):
-                sysinfo.append(line)
-        f.close()
-        if(sysinfo[1].strip() == driver.strip() and sysinfo[0].strip()==bios):
-            return BD_PREV_LOG;
-        else:
+class BDParse(BDError):
+    status = BDError()
+
+    def get_bd(self):
+        ct = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+
+        info = subprocess.run('systeminfo | findstr BIOS', shell=True, capture_output=True)
+        if info.returncode != 0:
+            print(ct+" :: Failed to execute: 'systeminfo' :: Exiting process")
+            return BD_ERROR_SUBPROCESS;
+        raw_bios = info.stdout.decode().split(":")
+        bios = raw_bios[1].strip()
+
+        info = subprocess.run('cd | dir',shell=True, cwd='Desktop\DRIVER',capture_output=True)
+        if info.returncode != 0:
+            print(ct+" :: Failed to execute: 'cd | dir :: Exiting process")
+            return BD_ERROR_SUBPROCESS;
+        raw_driver = info.stdout.decode().split("<DIR>")
+        driver = raw_driver[3].strip().split(" ")[0]
+
+        bios_s = "SBIOS  --- "+bios
+        driver_s = "DRIVER --- "+driver
+
+        return driver_s, bios_s;
+    
+    def check_bd(self, driver, bios):
+        try:
+            f = open("B-D_log.txt","r")
+            sysinfo = []
+            for element in range(0,4):
+                line = f.readline()
+                if(element>=2):
+                    sysinfo.append(line)
+            f.close()
+            if(sysinfo[1].strip() == driver.strip() and sysinfo[0].strip()==bios):
+                return status.PREV_LOG();
+            else:
+                return status.OK();
+
+        except FileNotFoundError:
+            print(datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")+" :: New log file created.")
+            f = open("B-D_log.txt","w")
+            return status.ERROR_EXIST();
+
+        return status.ERROR_UNKNOWN();
+
+    def write_bd(self,driver, bios):
+        ct = datetime.datetime.now()
+        try:
+            f = open("B-D_log.txt","r+")
+            data = f.read()
+            f.seek(0,0)
+            reason = "some reason"
+            f.write("----------------------------------------------------\n"+"TIME   --- "+ct.strftime("%m/%d/%Y, %H:%M:%S")+" --- \n"+bios+"\n"+driver+"----\nREASON: \n"+reason+"\n"+data)
+            f.close()
             return BD_OK;
 
-    except FileNotFoundError:
-        print(datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")+" :: New log file created.")
-        f = open("B-D_log.txt","w")
-        return BD_ERROR_EXIST;
+        except FileNotFoundError:
+            return status.ERROR_EXIST();
 
-    return BD_ERROR_UNKNOWN;
-
-def write_bd(driver, bios):
-    ct = datetime.datetime.now()
-    try:
-        f = open("B-D_log.txt","r+")
-        data = f.read()
-        f.seek(0,0)
-        reason = "some reason"
-        f.write("----------------------------------------------------\n"+"TIME   --- "+ct.strftime("%m/%d/%Y, %H:%M:%S")+" --- \n"+bios+"\n"+driver+"----\nREASON: \n"+reason+"\n"+data)
-        f.close()
-        return BD_OK;
-
-    except FileNotFoundError:
-        return BD_ERROR_EXIST;
-
-    return BD_ERROR_UNKNOWN;
-
-def get_bd():
-    ct = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
-
-    info = subprocess.run('systeminfo | findstr BIOS', shell=True, capture_output=True)
-    if info.returncode != 0:
-        print(ct+" :: Failed to execute: 'systeminfo' :: Exiting process")
-        return BD_ERROR_SUBPROCESS;
-    raw_bios = info.stdout.decode().split(":")
-    bios = raw_bios[1].strip()
-
-    info = subprocess.run('cd | dir',shell=True, cwd='Desktop\DRIVER',capture_output=True)
-    if info.returncode != 0:
-        print(ct+" :: Failed to execute: 'cd | dir :: Exiting process")
-        return BD_ERROR_SUBPROCESS;
-    raw_driver = info.stdout.decode().split("<DIR>")
-    driver = raw_driver[3].strip().split(" ")[0]
-
-    bios_s = "SBIOS  --- "+bios
-    driver_s = "DRIVER --- "+driver
-
-    return driver_s, bios_s;
+        return status.ERROR_UNKNOWN();
--- main.py
+++ main.py
@@ -1,37 +1,40 @@
 import bdparse
-from bdparse import check_bd, get_bd, write_bd, datetime
+# from bdparse import check_bd, get_bd, write_bd, datetime
 
 
 
 def main():
-    ct = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
-    try:
-        cur_driver, cur_bios = get_bd()
-        status = check_bd(cur_driver, cur_bios)
-        if(status==bdparse.BD_OK):
-            ct =datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
-            status=write_bd(cur_driver, cur_bios)
-            if(status==bdparse.BD_OK):
-                print(ct+" :: Successfully wrote SBIOS & DRIVER :: \n")
-            else:
-                print(ct+" :: Failed to write SBIOS & DRIVER :: Exiting process\n")
-                exit()
-        elif(status ==bdparse.BD_ERROR_EXIST):
-            ct =datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
-            status=write_bd(cur_driver, cur_bios)
-            if(status==bdparse.BD_OK):
-                print(ct+" :: Successfully wrote SBIOS & DRIVER :: \n")
-            else:
-                print(ct+" :: Failed to write SBIOS & DRIVER :: Exiting process\n")
-                exit()
-        elif(status==bdparse.BD_PREV_LOG):
-            print(ct+" :: SBIOS & DRIVER already logged :: \n")
-        else:
-            print(ct+" :: Unknown error :: Exiting proccess\n")
-            exit()
-    except TypeError:
-        print(ct+" :: Failed to retrive SBIOS or DRIVER :: Exiting proccess")
-        exit()
+
+    bd = bdparse.BDParse()
+    c_driver, c_bios = bd.get_bd()
+    # ct = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    # try:
+    #     cur_driver, cur_bios = get_bd()
+    #     status = check_bd(cur_driver, cur_bios)
+    #     if(status==bdparse.BD_OK):
+    #         ct =datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    #         status=write_bd(cur_driver, cur_bios)
+    #         if(status==bdparse.BD_OK):
+    #             print(ct+" :: Successfully wrote SBIOS & DRIVER :: \n")
+    #         else:
+    #             print(ct+" :: Failed to write SBIOS & DRIVER :: Exiting process\n")
+    #             exit()
+    #     elif(status ==bdparse.BD_ERROR_EXIST):
+    #         ct =datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    #         status=write_bd(cur_driver, cur_bios)
+    #         if(status==bdparse.BD_OK):
+    #             print(ct+" :: Successfully wrote SBIOS & DRIVER :: \n")
+    #         else:
+    #             print(ct+" :: Failed to write SBIOS & DRIVER :: Exiting process\n")
+    #             exit()
+    #     elif(status==bdparse.BD_PREV_LOG):
+    #         print(ct+" :: SBIOS & DRIVER already logged :: \n")
+    #     else:
+    #         print(ct+" :: Unknown error :: Exiting proccess\n")
+    #         exit()
+    # except TypeError:
+    #     print(ct+" :: Failed to retrive SBIOS or DRIVER :: Exiting proccess")
+    #     exit()
 
 
     return 0;
